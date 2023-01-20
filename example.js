ef4de38266010f37d92f67b7f1278c6c92a740a2
JatinBhuva <70341139+JatinBhuva@users.noreply.github.com>
2023-01-20 14:24:54 +0530

    Delete example.js

--- example.js
+++
@@ -1,7 +0,0 @@
-var a=document.getElementsByTagName('h1');
-var b=document.getElementById('num');
-var c=document.getElementsByClassName('para');
-c[0].innerText="Do you really want to chage is????"
-console.log(a[0].innerHTML);
-console.log(b.placeholder);
-console.log(c[0].placeholder);
